6a0aa2609e45ba91e2fe4864e1b7f8e207561b79
Alexei Vasilkov <alexei.vasilkov@gmail.com>
2013-07-20 00:32:19 +0300

    to access string.xml res need context.getString()

--- src/com/dzebsu/acctrip/adapters/EventListViewAdapter.java
+++ src/com/dzebsu/acctrip/adapters/EventListViewAdapter.java
@@ -49,8 +49,8 @@ public class EventListViewAdapter extends ArrayAdapter<Event> {
 		RowViewHolder holder = (RowViewHolder) rowView.getTag();
 		holder.name.setText(objects.get(position).getName());
 		holder.desc.setText(objects.get(position).getDesc());
-		holder.operationsCnt.setText(com.dzebsu.acctrip.R.string.ops_text_view+"25");
-		holder.eventId.setText(com.dzebsu.acctrip.R.string.id_text_view+((Long)objects.get(position).getId()).toString());
+		holder.operationsCnt.setText(context.getString(com.dzebsu.acctrip.R.string.ops_text_view)+"25");
+		holder.eventId.setText(context.getString(com.dzebsu.acctrip.R.string.id_text_view)+((Long)objects.get(position).getId()).toString());
 		
 /*		 LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		 View row = inflater.inflate(com.dzebsu.acctrip.R.layout.row_event_list, parent,false);
